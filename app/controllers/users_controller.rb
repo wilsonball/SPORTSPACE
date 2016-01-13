c85903965a940596508d1a1598e03149a6fc232f
Wilson Muh <wilson.muh@gmail.com>
2016-01-12 20:29:22 -0800

    trying to fix run_path bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,6 @@
 class UsersController < ApplicationController
 	def show
 		@user = User.find(params[:id])
-		@court = Court.find(params[:id])
 		@courts = @user.courts
 		@runs = @user.runs
 		@photos = @user.photos
