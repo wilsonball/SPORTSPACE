23dce909cea7b41adcc04f70b16b5ca0a5258ebb
Wilson Muh <wilson.muh@gmail.com>
2016-01-13 13:23:30 -0800

    testing user location java

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,7 +6,6 @@ class PagesController < ApplicationController
 
   def search
     @search = Court
-      .where(is_active: true)
       .joins('LEFT OUTER JOIN runs ON runs.court_id = courts.id')
       .select("courts.*, count(runs.id) as num_runs")
       .group("courts.id")
@@ -38,7 +37,7 @@ class PagesController < ApplicationController
 
     arrResult = Array.new
 
-  	@courts = @search.result.paginate(:page => params[:page], :per_page => 5)
+  	@courts = @search.result.paginate(:page => params[:page], :per_page => 5, :total_entries => 500)
     # @search_name = @courts_listing_name.ransack(params[:q])
 
   	@arrCourts = @courts.to_a
--- app/models/court.rb
+++ app/models/court.rb
@@ -16,11 +16,19 @@ class Court < ActiveRecord::Base
 		reviews.count == 0 ? 0 : reviews.average(:star).round(2)
 	end
 
-	def show_first_photo
+	def show_first_photo 
 		if photos.length == 0
 			'http://www.mckearneyasphalt.com/images/basketball-court/basketball-court-construction-2-thumb.jpg'
 		else
 			photos[0].image.url(:thumb)
+		end
+	end
+
+	def show_first_photo_medium
+		if photos.length == 0
+			'http://www.mckearneyasphalt.com/images/basketball-court/basketball-court-construction-2-thumb.jpg'
+		else
+			photos[0].image.url(:medium)
 		end
 	end
 
