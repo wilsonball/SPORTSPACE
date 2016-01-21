01d19b77fab87e751b7f88133324e9bd463bd8e6
Wilson Muh <wilson.muh@gmail.com>
2016-01-20 18:57:41 -0800

    fixed user#show runs/upcoming render and added if else for seengames and seenplayers in court#show

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,6 @@
 class UsersController < ApplicationController
 	def show
 		@user = User.find(params[:id])
-		@courts = @user.courts
-		@runs = @user.runs
 		@userphotos = @user.userphotos
 	end
 end
