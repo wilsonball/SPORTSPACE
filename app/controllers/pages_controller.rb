92ad0bbef34929cc37479ccc4fa2aeb442529f40
Wilson Muh <wilson.muh@gmail.com>
2016-01-27 01:06:03 -0800

    if no geolocation then search by manual address

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,15 +9,42 @@ class PagesController < ApplicationController
       .joins('LEFT OUTER JOIN runs ON runs.court_id = courts.id')
       .select("courts.*, count(runs.id) as num_runs")
       .group("courts.id")
-  	if params[:address].present? && params[:address].strip != ""
-      if params[:address] =~ /-?\d+(\.\d+)?,-?\d+(\.\d+)?/
-        location = params[:address].split(',').map{|num| Float(num)}
-      else
-        location = params[:address]
-      end
 
-      @search = @search.near(location, 25, order: 'distance')
-  	end
+
+    if params[:coords].present? && params[:coords].strip != ""
+      if params[:coords] =~ /-?\d+(\.\d+)?,-?\d+(\.\d+)?/
+        location = params[:coords].split(',').map{|num| Float(num)}
+        @search = @search.near(location, 25, order: 'distance')
+      end
+    else
+      @search = @search.near(params[:address], 25, order: 'distance')
+      
+    end
+
+
+
+  	# if params[:address].present? && params[:address].strip != ""
+   #    if params[:address] =~ /-?\d+(\.\d+)?,-?\d+(\.\d+)?/
+   #      location = params[:address].split(',').map{|num| Float(num)}
+   #    else
+   #      if params[:coords] =~ /-?\d+(\.\d+)?,-?\d+(\.\d+)?/
+   #      location = params[:coords].split(',').map{|num| Float(num)}
+   #      else
+   #        location = params[:coords]
+   #      end
+   #    end
+   #    @search = @search.near(location, 25, order: 'distance')
+  	# end
+
+    # if params[:coords].present? && params[:coords].strip != ""
+    #   if params[:coords] =~ /-?\d+(\.\d+)?,-?\d+(\.\d+)?/
+    #     location = params[:coords].split(',').map{|num| Float(num)}
+    #   else
+    #     location = params[:coords]
+    #   end
+
+    #   @search = @search.near(location, 25, order: 'distance')
+    # end
 
     if params[:time].present? && params[:time].strip != ""
       @search = @search
